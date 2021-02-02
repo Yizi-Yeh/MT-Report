a112013f9b76eedf9e91e7dd1b10aaccf938624b
Yizi-Yeh <g384357@gmail.com>
2021-02-02 16:29:39 +0800

    註冊登入_文字
    註冊登入_文字

--- MTReport/front/src/components/members/Log.vue
+++ MTReport/front/src/components/members/Log.vue
@@ -6,16 +6,16 @@
     <div class="col-12 mt-5 d-flex flex-column align-items-center">
             <h2 text-center> Feel free to join MT10x10</h2>
             <p>Are you ready to have a fantastic hiking with us?</p>
- <form class="form-signin rounded border d-flex flex-column align-items-center" @submit.prevent="signin" >
+ <form class="form-signin rounded border d-flex flex-column align-items-center" >
   <img class="mb-3 mt-5"  src="https://mt10x10.files.wordpress.com/2020/07/cropped-e7b6b2e7ab99e6a899e9a18c-e6a899e8aa8c-4.jpg" alt="" >
   <h4 class="mb-2">MT會員登入</h4>
   <label for="inputEmail" class="sr-only mb-2">Email address</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email address" v-model="account" :state="accountState" required autofocus>
   <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Password" v-model="password" :state="passwordState" required>
+  <input type="password" id="inputPassword" class="form-control" placeholder="Password" v-model="password" :state="passwordState" required >
   <div class="checkbox mb-3">
   </div>
-  <button class="btn btn-lg btn-dark btn-block" type="submit" @click="onSubmit"> Signin</button>
+  <button class="btn btn-lg btn-dark btn-block" type="submit" @click="onSubmit">Signin</button>
   <button class="btn btn-lg btn-dark btn-block" type="reset" @reset="onReset"> Reset</button>
   <div class="row">
     <div class="col-12 mt-4 mr-0">
