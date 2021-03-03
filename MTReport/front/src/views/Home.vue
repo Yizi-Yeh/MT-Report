e1a83d83fbaf7a23fb5410fc43001ec128157277
Yizi-Yeh <g384357@gmail.com>
2021-03-03 17:42:21 +0800

    skrollr滾動效果

--- MTReport/front/src/views/Home.vue
+++ MTReport/front/src/views/Home.vue
@@ -2,34 +2,39 @@
 <div>
     <Navbar/>
       <Carousel/>
+      
         <!-- 近期開團 -->
         <transition name="slide-left"></transition>
         <section id="section02">
           <div class="container">
            <p class="act-title">近期開團</p>
-              <div class="row h-100">
-              <div id="newplani" class="swiper-slide col-lg-4 col-xs-12 col-sm-12 d-flex" v-for="(item) in newplanswipersort" :key="item._id" >
+              <div class="row h-100 ">
+              <div  class="swiper-slide col-lg-4 col-md-10 col-sm-12 d-flex flex-lg-row" v-for="(item) in newplanswipersort" :key="item._id" >
             <a href="#"  @click.prevent="getNewPlan(item._id)">      
-              <div class="act-view mb-3 rounded">
-            <img class="act-img" :src="item.p_id.images[0].imgUrl">
-              <div class="text">     
+              <div data-0="transform:translateX(-400px);" data-top=";transform:translateX(0px);"  class="mb-1 rounded">
+            <img  class="act-img act-view act-re-view rounded" :src="item.p_id.images[0].imgUrl">
+              <div class="text mt-2">
+                <p class="list_category"></p>   
             <h5 class="act-text">{{ item.p_id.title }}</h5>
             <h5 class="act-text">活動日期：{{ item.date }}</h5>
-             <h5 class="act-text">分類：{{ item.p_id.category }}</h5>
-            <h5  class="act-text">NT{{ item.price | commaFormat | dollarSign }} </h5>
+             <p class="act-text-category rounded">分類：{{ item.p_id.category }}</p>
+            <!-- <h5  class="act-text">NT{{ item.price | commaFormat | dollarSign }} </h5> -->
                </div>
-             
              </div>
           </a>
         </div>
     </div>
  </div>
+           <div class="btn-wrap">
+              <a class="btn-view btn-outline-info rounded btn-sm">
+               <router-link class="nav-link" to="/newplan">查看更多</router-link></a>
+          </div>
     </section>
 
     <section id="section03" >
    <div class="container-full  container-banner">
       <div class="container">
-        <h2 class="container-banner-title">Welcome to join MT10x10 and follow us!</h2>
+        <h2 class="container-banner-title">Welcome to join MT10x10 and<br> follow us!</h2>
         <div class="container-banner-sub-title">|登山⛰|露營⛺️|健行🥾|戶外探索🗻｜</div>
       </div>
     </div>
@@ -40,14 +45,17 @@
     <div v-animate-onscroll="{down: 'animated zoomIn 5s', up: 'animated zoomIn 5s' }">
     <div class="container-intro">
         <h5 class="container-intro-title">MT10x10登山社<br><span class="container-intro-subtitle">HIKING CLUB</span></h5>
-       <h4 class="container-intro-sub">展現渴望自由的你<br>Show your craving for freedom.</h4>
-    <div class="col-8 mx-auto">
+    <div class="col-12 col-md-10 mx-auto">
        <span class="container-intro-sub-txt">MT10x10（山來水去企業社）<br>
         象徵著台灣最具代表性的一百座山岳，也表達我們對台灣山林的敬愛之情。<br>我們是由熱血的年輕人所創建的登山社，
         <br class="container-intro-sub-txt">用10分熱情與10分愛山林的心，和山友們一步步一同完成百岳，<br>帶領更多人瞭解台灣的美。 <br><br>然而，一提到日本就想到莊嚴的富士山；<br>尼泊爾有崇高的聖母峰；瑞士有秀麗的阿爾卑斯山；<br>我們希望拓展台灣山域風景知名度，讓世界一提到台灣就想到有壯闊的玉山。<br><br>MT10x10登山社（山來水去企業社）歡迎您</span>
    </div>
     </div>
     </div>
+               <div class="btn-wrap">
+              <a class="btn-view btn-outline-info rounded btn-sm">
+               <router-link class="nav-link" to="/brand">關於MT</router-link></a>
+          </div>
   </section>
     <br>
 
@@ -58,19 +66,19 @@
         <div class="row h-100 rounded">
            <swiper :options="swiperOption">
           <swiper-slide
-         class="col-lg-4 d-flex"  v-for="r in recommends" :key="r.id" >
+         class="col-lg-4 col-md-6  d-flex"  v-for="r in recommends" :key="r.id" >
           <a href="#"  @click.prevent="getProduct(r._id)">
         <div class="act-re-view rounded">
             <img style="height:250px;background-size:cover; background-position:center" :src="r.images[0].imgUrl">
               <div class="text">     
-            <h2 class="act-text">{{ r.title }}</h2>
+            <h2 class="act-text mt-3">{{ r.title }}</h2>
               <!-- <span class="act-text">分類：{{ r.category }}</span>
             <div  class="act-text">NT{{ r.cost | commaFormat | dollarSign }} </div> -->
                 </div>
               </div>
           </a> 
          </swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div> 
+        <div class="swiper-pagination " slot="pagination"></div> 
            </swiper>
          </div>
          </div>
@@ -88,7 +96,9 @@ import store from '@/store'
 import Axios from 'axios'
 import 'swiper/swiper-bundle.css' // require styles
 import { swiper, swiperSlide } from "vue-awesome-swiper";
-
+require(['skrollr'], function(skrollr){
+    const s = skrollr.init();
+});
 
 export default {
   data() {
@@ -101,20 +111,19 @@ export default {
           delay: 2000,
         },
         pagination: {
-          
           el: '.swiper-pagination',
           clickable: true
         },
         breakpoints: {
-          480: {
+          576: {
             slidesPerView: 1,
             spaceBetween: 20
           },
-          640: {
+          768: {
             slidesPerView: 2,
-            spaceBetween: 30
+            spaceBetween:10
           },
-          800: {
+          992: {
             slidesPerView: 3,
             spaceBetween: 30
           }
@@ -146,7 +155,7 @@ export default {
     },
     recommends () {
       const products = [...this.products]
-      return products.sort(() => Math.random() - 0.5).splice(0,9)
+      return products.sort(() => Math.random() - 0.5).splice(0,8)
     },
     
   },
